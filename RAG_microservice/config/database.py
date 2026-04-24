9c2f9273a4558c14f2c6d3710b34600003161262
Aman Yadav <amanyadav2303@gmail.com>
2026-04-24 06:59:21 +0530

    fixes

--- RAG_microservice/config/database.py
+++ RAG_microservice/config/database.py
@@ -1,13 +1,14 @@
 from pydantic_settings import BaseSettings
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
-from ..models.universityInfo import UniversityInfo
+from models.universityInfo import UniversityInfo
 
 class Settings(BaseSettings):
     MONGODB_URI : str
     DB_NAME : str = "Mentora"
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 settings = Settings()
 
@@ -17,12 +18,18 @@ DOCUMENT_MODELS = [
 ]
 
 async def connectDB():
-    client = AsyncIOMotorClient(settings.MONGODB_URI)
-    await init_beanie(
-        database=client[settings.DB_NAME],
-        document_models=DOCUMENT_MODELS
-    )
-    return client
+    try:
+        client = AsyncIOMotorClient(settings.MONGODB_URI)
+        await init_beanie(
+            database=client[settings.DB_NAME],
+            document_models=DOCUMENT_MODELS
+        )
+        print("✅ MongoDB connected successfully")
+        return client
+    except Exception as e:
+        print(f"❌ MongoDB connection failed: {e}")
+        raise
 async def disconnectDB(client: AsyncIOMotorClient):
     client.close()
+    print("🔌 MongoDB disconnected")
     

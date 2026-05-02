daaf6b150b4628808a41befdf571f79bab286fa1
Aman Yadav <amanyadav2303@gmail.com>
2026-05-02 23:30:38 +0530

    flutter client app and node gateway server deprecated

--- node_gateway/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const PORT = 7001;
-const URI = "mongodb+srv://ay0233003:5WLgBLn5KQn50l9U@hideinhue.hdf5rva.mongodb.net/HideInHue";
-const JWT_PRIVATE_KEY = "@HIDEINHUE$2025"
-
-module.exports={
-    PORT,
-    URI,
-    JWT_PRIVATE_KEY
-};
